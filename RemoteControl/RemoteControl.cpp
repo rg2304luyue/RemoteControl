20492f60ea86a122e9b7b1a4f46d7b5a1fb4c195
mineheartx <2779902707@qq.com>
2026-04-21 15:06:57 +0800

    server处理屏幕+client改造为窗口程序

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -2,9 +2,9 @@
 
 #include <stdio.h>
 #include <Windows.h> // windows API
-
+#include <atlimage.h> // 获取屏幕
+#include <ShellScalingApi.h> // 获取屏幕 DPI
 #pragma comment(lib, "ws2_32.lib") // 链接ws2_32.lib库
-
 #define RECV_BUFFER_SIZE 1024 * 1024 * 1
 
 // 将这个结构体按1字节对齐
@@ -21,84 +21,161 @@ struct Packet {
 	char body[];         // 包数据，不固定长度
 };
 
+// 枚举类型
+enum CMD {
+	CMD_SCREEN = 1,			// 发送屏幕命令
+	CMD_MOUSE = 2,			// 鼠标命令
+	CMD_KEYBOARD = 4,       // 键盘命令
+	CMD_TESTCONNECT = 2026, // 测试连接命令
+};
+
 int GetPacketLen(Packet* pck);
 Packet* PackPacket(int cmd, char* buffer, int buffer_len);
 Packet* ParsePacket(char* buffer, int len);
+int InitServer();
+int HandleCommand(Packet* packet);
+int HandleScreen(Packet* packet);
+int HandleMouse(Packet* packet);
+int HandleKeyboard(Packet* packet);
+int HandleTestConnect(Packet* packet);
+SOCKET g_server_socket;
+SOCKET g_client_socket;
 
 int main() {
-	// 服务器网络编程步骤
-	// 1. 初始化网络环境
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-	// 2. 创建服务器socket AF_INET: IPv4协议，SOCK_STREAM: TCP协议，0: 默认协议 SOCKET_DGRAM: UDP协议
-	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == INVALID_SOCKET) {
-		printf("创建服务器socket失败\r\n");
-		return 0;
-	}
-	// 3. 给服务器socket绑定IP和端口
-	// 准备一个地址
-	SOCKADDR_IN server_addr;
-	server_addr.sin_family = AF_INET; // IPv4协议
-	server_addr.sin_port = ntohs(9999); // 0~65535，0~1023为系统保留端口，1024~49151为注册端口，49152~65535为动态/私有端口
-	// inet_addr函数将点分十进制的IP地址转换为网络字节序的二进制形式
-	server_addr.sin_addr.S_un.S_addr = inet_addr("100.64.54.246"); // 0.0.0.0 监听服务器上的所有ip
-	if (bind(server_socket, (sockaddr*)&server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR){
-		printf("绑定服务器socket失败\r\n");
+	if (InitServer() != 0) {
+		printf("初始化服务器失败\r\n");
 		return 0;
 	}
 
-	// 4. 开启服务器socket监听 backlog:允许完成三次握手的客户端数量
-	if (listen(server_socket, 1) == SOCKET_ERROR) {
-		printf("监听服务器socket失败\r\n");
-		return 0;
+	//// 5. 接受客户端连接，会返回客户端socket
+	//SOCKADDR_IN client_addr;
+	//int client_addr_len = sizeof(SOCKADDR_IN);
+	//printf("等待客户端连接...\r\n");
+	//// 阻塞等待客户端连接
+	//SOCKET client_socket = accept(g_server_socket, (sockaddr*)&client_addr, &client_addr_len);
+	//printf("客户端连接成功\r\n");
+
+	//// 6. 处理客户端请求
+	//char* buffer = (char*)malloc(RECV_BUFFER_SIZE);
+	//// 记录缓冲区当前数据的长度
+	//static int index = 0;
+
+	//while (true) { // 循环处理客户端请求，直到客户端断开连接
+	//	// 返回客户端发送的数据，阻塞等待客户端发送数据
+	//	// RECV_BUFFER_SIZE - index 缓冲区大小
+	//	int len = recv(client_socket, buffer + index, RECV_BUFFER_SIZE - index, 0);
+	//	index += len; // index = 5
+
+	//	// 依照协议解析数据，把数据读出来
+	//	// 把缓冲区总长度传入
+	//	Packet* packet = ParsePacket(buffer, index);
+	//	index -= GetPacketLen(packet);
+	//	memmove(buffer, buffer + GetPacketLen(packet), index);
+	//	// 一个可持续缓冲区就准备好了
+
+	//	HandleCommand(packet);
+	//	free(packet);
+	//}
+	//// 关闭套接字
+	//closesocket(client_socket);
+	//closesocket(g_server_socket);
+	//// 清除
+	//WSACleanup();
+	HandleScreen(NULL);
+	return 0;
+}
+
+int HandleCommand(Packet* packet) {
+	int ret = 0;
+	switch (packet->header.cmd)
+	{
+	case CMD_SCREEN: // 发送屏幕命令
+		ret = HandleScreen(packet);
+		break;
+	case CMD_MOUSE: // 鼠标命令
+		ret = HandleMouse(packet);
+		break;
+	case CMD_KEYBOARD: // 键盘命令
+		ret = HandleKeyboard(packet);
+		break;
+	case CMD_TESTCONNECT: // 测试连接命令
+		ret = HandleTestConnect(packet);
+		break;
+	default:
+		break;
 	}
+	return ret;
+}
 
-	// 5. 接受客户端连接，会返回客户端socket
-	SOCKADDR_IN client_addr;
-	int client_addr_len = sizeof(SOCKADDR_IN);
-	printf("等待客户端连接...\r\n");
-	// 阻塞等待客户端连接
-	SOCKET client_socket = accept(server_socket, (sockaddr*)&client_addr, &client_addr_len);
-	printf("客户端连接成功\r\n");
-
-	// 6. 处理客户端请求
-	char* buffer = (char*)malloc(RECV_BUFFER_SIZE);
-	// 记录缓冲区当前数据的长度
-	static int index = 0;
-
-	while (true) { // 循环处理客户端请求，直到客户端断开连接
-		// 返回客户端发送的数据，阻塞等待客户端发送数据
-		// RECV_BUFFER_SIZE - index 缓冲区大小
-		int len = recv(client_socket, buffer + index, RECV_BUFFER_SIZE - index, 0);
-		index += len; // index = 5
-
-		// 依照协议解析数据，把数据读出来
-		// 把缓冲区总长度传入
-		Packet* packet = ParsePacket(buffer, index); // 假设解析了3个字节
-		index -= GetPacketLen(packet);
-		memmove(buffer, buffer + GetPacketLen(packet), index);
-		// 一个可持续缓冲区就准备好了
-		printf("server recive data：%s\r\n", packet->body);
-		printf("server recive packet->header.magic：%x\r\n", packet->header.magic);
-		printf("server recive packet->header.cmd：%d\r\n", packet->header.cmd);
-		printf("server recive packet->header.body_len：%d\r\n", packet->header.body_len);
-		printf("----------");
-
-		// 7. 发送数据
-		Packet* pck = PackPacket(packet->header.cmd, packet->body, packet->header.body_len);
+int HandleScreen(Packet* packet) {
+	// 获取本地屏幕
+	// 创建一个image对象
+	CImage image;
+	// 获取屏幕DC NULL代表全局屏幕
+	HDC hScreenDC = GetDC(NULL);
+	// 获取屏幕像素位宽
+	int bitWidth = GetDeviceCaps(hScreenDC, BITSPIXEL);
+	printf("屏幕像素位宽：%d\r\n", bitWidth);
+	// 开启DPI感知，获取屏幕的物理分辨率
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+	// 获取屏幕宽高
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	// w: 1536 h: 864 这里拿到的是逻辑分辨率
+	image.Create(screenWidth, screenHeight, bitWidth);
+	// 把屏幕DC的数据拷贝到image对象中
+	BitBlt(image.GetDC(), 0, 0, screenWidth, screenHeight, hScreenDC, 0, 0, SRCCOPY);
+	// 释放屏幕上下文
+	ReleaseDC(NULL, hScreenDC);
+	// 把image对象的数据打包成数据包
+	// image.Save("test.png", ::Gdiplus::ImageFormatPNG);
+	// 转为一个网络流
+	// 从堆上申请一块可变化的空间
+	HGLOBAL hMen = GlobalAlloc(GMEM_MOVEABLE, 0);
+	if (hMen == NULL) {
+		// 分配内存失败
+		return -1;
+	}
+	// 创建一个内存流
+	IStream* pStream = NULL;
+	HRESULT ret = CreateStreamOnHGlobal(hMen, true, &pStream);
+	if (ret == S_OK) {
+		// 将文件保存到内存流 schar char pStream = echar
+		image.Save(pStream, ::Gdiplus::ImageFormatPNG);
+		// 将流指针放到开头 pStream = schar STREAM_SEEK_SET：开头
+		LARGE_INTEGER lg = { 0 };
+		pStream->Seek(lg, STREAM_SEEK_SET, NULL);
+		// 获取这个流指针
+		char* pdata = (char*)GlobalLock(hMen);
+		// 获取流长度
+		int len = GlobalSize(hMen);
+		// 发送数据
+		Packet* packet = PackPacket(CMD_SCREEN, pdata, len);
+		send(g_client_socket,(char*) & packet->header.magic, sizeof(PacketHeader) + len, 0);
 		free(packet);
-		send(client_socket, (char*)&pck->header.magic, GetPacketLen(pck), 0);
-		printf("服务器发送数据：%s\r\n", buffer);
-		// 模拟服务器处理命令耗时过程
-		//Sleep(500);
+		// 解锁内存
+		GlobalUnlock(hMen);
 	}
-	// 关闭套接字
-	closesocket(client_socket);
-	closesocket(server_socket);
-	// 清除
-	WSACleanup();
+	// 释放流指针
+	pStream->Release();
+	// 释放全局内存
+	GlobalFree(hMen);
+	// 释放图像DC
+	image.ReleaseDC();
+	// 通过网络发送
+
+	return 0;
+}
+int HandleMouse(Packet* packet) {
+	// 处理鼠标命令
+	return 0;
+}
+int HandleKeyboard(Packet* packet) {
+	// 处理键盘命令
+	return 0;
+}
+int HandleTestConnect(Packet* packet) {
+	// 处理测试连接命令
 	return 0;
 }
 
@@ -150,4 +227,37 @@ Packet* ParsePacket(char* buffer, int len) {
 		memcpy(&ppck->header, &pck.header, sizeof(PacketHeader));
 		return ppck;
 	}
+}
+
+int InitServer() {
+	// 服务器网络编程步骤
+	// 1. 初始化网络环境
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	// 2. 创建服务器socket AF_INET: IPv4协议，SOCK_STREAM: TCP协议，0: 默认协议 SOCKET_DGRAM: UDP协议
+	g_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (g_server_socket == INVALID_SOCKET) {
+		printf("创建服务器socket失败\r\n");
+		return -1;
+	}
+	// 3. 给服务器socket绑定IP和端口
+	// 准备一个地址
+	SOCKADDR_IN server_addr;
+	server_addr.sin_family = AF_INET; // IPv4协议
+	server_addr.sin_port = ntohs(9999); // 0~65535，0~1023为系统保留端口，1024~49151为注册端口，49152~65535为动态/私有端口
+	// inet_addr函数将点分十进制的IP地址转换为网络字节序的二进制形式
+	server_addr.sin_addr.S_un.S_addr = inet_addr("100.64.54.246"); // 0.0.0.0 监听服务器上的所有ip
+	if (bind(g_server_socket, (sockaddr*)&server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) {
+		printf("绑定服务器socket失败\r\n");
+		return -2;
+	}
+
+	// 4. 开启服务器socket监听 backlog:允许完成三次握手的客户端数量
+	if (listen(g_server_socket, 1) == SOCKET_ERROR) {
+		printf("监听服务器socket失败\r\n");
+		return -3;
+	}
+
+	return 0;
 }
--- client/client.cpp
+++ client/client.cpp
@@ -1,8 +1,6 @@
 ﻿#include <stdio.h>
 #include <Windows.h> // windows API
-
 #pragma comment(lib, "ws2_32.lib") 
-
 #define RECV_BUFFER_SIZE 1024 * 1024 * 1
 
 // 将这个结构体按1字节对齐
@@ -21,65 +19,120 @@ struct Packet {
 
 Packet* PackPacket(int cmd, char* buffer, int buffer_len);
 Packet* ParsePacket(char* buffer, int len);
-
-int main() {
-	// 客户端网络编程步骤
-	// 1. 初始化socket链接
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
-	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == INVALID_SOCKET) {
-		printf("创建服务器socket失败\r\n");
-		return 0;
+int InitSocket();
+SOCKET g_server_socket;
+SOCKADDR_IN g_server_addr;
+LRESULT CALLBACK winProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam); // 默认处理其他消息
 	}
-	SOCKADDR_IN server_addr;
-	server_addr.sin_family = AF_INET; // IPv4协议
-	server_addr.sin_port = ntohs(9999); // 转为网络字节序
-	server_addr.sin_addr.S_un.S_addr = inet_addr("100.64.54.246"); // 服务器IP地址
-	
-	// 2. 连接服务器
-	if (connect(server_socket, (sockaddr*)&server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) {
-		printf("连接服务器失败\r\n");
-		return 0;
-	}
-	printf("连接服务器成功\r\n");
-
-	// 3. 发送数据
-	char buffer[1024];
-	char* recv_buffer = (char*)malloc(RECV_BUFFER_SIZE);
-	int count = 0; // 记录发送数据的次数
-	while (true) {
-		count++;
-		// 准备发送数据 sprintf: 把一个格式化字符串写入缓冲区
-		//sprintf_s(buffer, "packet:%d", count);
-		// 接收用户输入 stdin: 标准输入，stdout: 标准输出，stderr: 标准错误输出
-		printf("请输入要发送的数据：");
-		fgets(buffer, 1024, stdin);
-
-		Packet* send_packet = PackPacket(2000, buffer, 10);
-		send(server_socket, (char*)&send_packet->header.magic, send_packet->header.body_len + sizeof(PacketHeader), 0);
-		// 释放内存
-		free(send_packet);
-		printf("client send data：%s\r\n", buffer);
-
-		// 等待服务器处理命令，模拟服务器处理命令耗时过程
-		int len = recv(server_socket, recv_buffer, RECV_BUFFER_SIZE, 0);
-		if(len > 0) {
-			Packet* recv_pck = ParsePacket(recv_buffer, len);
-			printf("server recive data：%s\r\n", recv_pck->body);
-			printf("server recive packet->header.magic：%x\r\n", recv_pck->header.magic);
-			printf("server recive packet->header.cmd：%d\r\n", recv_pck->header.cmd);
-			printf("server recive packet->header.body_len：%d\r\n", recv_pck->header.body_len);
-			free(recv_pck);
-		}
-		// Sleep可以让程序休眠
-		//Sleep(10);
-	}
-	
-	closesocket(server_socket);
-	WSACleanup();
 	return 0;
 }
+
+// 创建窗口的入口函数
+int WINAPI WinMain(
+	HINSTANCE hInstance,        // 当前实例的句柄
+	HINSTANCE hPreventInstance, // 前一个实例的句柄，Windows 95/98/Me不使用，始终为NULL
+	LPSTR pCmdLine,             // 命令行参数
+	int nCmdShow) {             // 显示窗口的方式
+	// 1. 注册一个窗口类
+	WNDCLASS ws = {};
+	LPCWSTR CLASS_NAME = L"MainWindow";
+	LPCWSTR WINDOW_NAME = L"远程控制";
+	ws.lpfnWndProc = winProc; // 指定窗口过程函数
+	ws.hInstance = hInstance; // 指定当前实例的句柄
+	ws.lpszClassName = CLASS_NAME; // 指定窗口类的名称
+	ws.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // 指定窗口背景颜色
+	ws.hCursor = LoadCursor(NULL, IDC_ARROW); // 指定窗口光标
+	ws.hIcon = LoadIcon(NULL, IDI_APPLICATION); // 指定窗口图标
+	ws.style = CS_HREDRAW | CS_VREDRAW; // 指定窗口样式，CS_HREDRAW: 水平重绘，CS_VREDRAW: 垂直重绘
+	if(!RegisterClass(&ws)) {
+		MessageBox(NULL, L"窗口注册失败", L"错误", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	// 2. 创建窗口
+	HWND hwnd = CreateWindow(
+		CLASS_NAME, // 窗口类名称
+		WINDOW_NAME, // 窗口标题
+		WS_OVERLAPPEDWINDOW, // 窗口样式，WS_OVERLAPPEDWINDOW: 标准窗口
+		CW_USEDEFAULT, CW_USEDEFAULT, // 窗口坐标x，y的位置
+		600, 400, // 窗口宽高
+		NULL, // 父窗口句柄
+		NULL, // 菜单句柄
+		hInstance, // 当前实例的句柄
+		NULL, // 额外参数
+		);
+	if (hwnd == NULL) {
+		// 弹出选项框
+		MessageBox(NULL, L"窗口创建失败", L"错误", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	// 3. 显示窗口
+	ShowWindow(hwnd, nCmdShow);
+	// 4. 更新窗口
+	UpdateWindow(hwnd);
+	// 5. 创建消息循环, while不停的绘制
+	MSG msg = {0};
+	while (GetMessage(&msg, NULL, 0, 0)) { // 阻塞获取消息
+		// 翻译消息，DispatchMessage会调用窗口过程函数winProc
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
+
+// main函数是控制台的入口
+//int main() {
+//	//// 2. 连接服务器
+//	//if (connect(g_server_socket, (sockaddr*)&g_server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) {
+//	//	printf("连接服务器失败\r\n");
+//	//	return 0;
+//	//}
+//	//printf("连接服务器成功\r\n");
+//
+//	//// 3. 发送数据
+//	//char buffer[1024];
+//	//char* recv_buffer = (char*)malloc(RECV_BUFFER_SIZE);
+//	//int count = 0; // 记录发送数据的次数
+//	//while (true) {
+//	//	count++;
+//	//	// 准备发送数据 sprintf: 把一个格式化字符串写入缓冲区
+//	//	//sprintf_s(buffer, "packet:%d", count);
+//	//	// 接收用户输入 stdin: 标准输入，stdout: 标准输出，stderr: 标准错误输出
+//	//	printf("请输入要发送的数据：");
+//	//	fgets(buffer, 1024, stdin);
+//	//	// 获取屏幕
+//	//	Packet* send_packet = PackPacket(1, buffer, 10);
+//	//	send(g_server_socket, (char*)&send_packet->header.magic, send_packet->header.body_len + sizeof(PacketHeader), 0);
+//	//	// 释放内存
+//	//	free(send_packet);
+//	//	printf("client send data：%s\r\n", buffer);
+//
+//	//	// 等待服务器处理命令，模拟服务器处理命令耗时过程
+//	//	int len = recv(g_server_socket, recv_buffer, RECV_BUFFER_SIZE, 0);
+//	//	if(len > 0) {
+//	//		Packet* recv_pck = ParsePacket(recv_buffer, len);
+//	//		printf("client recive data：%s\r\n", recv_pck->body);
+//	//		printf("client recive packet->header.magic：%x\r\n", recv_pck->header.magic);
+//	//		printf("client recive packet->header.cmd：%d\r\n", recv_pck->header.cmd);
+//	//		printf("client recive packet->header.body_len：%d\r\n", recv_pck->header.body_len);
+//	//		if (recv_pck->header.cmd == 1) {
+//	//			// 服务器返回了屏幕数据
+//	//			// 解析显示数据
+//	//			
+//	//		}
+//	//		free(recv_pck);
+//	//	}
+//	//	// Sleep可以让程序休眠
+//	//	//Sleep(10);
+//	//}
+//	//
+//	//closesocket(g_server_socket);
+//	//WSACleanup();
+//	//return 0;
+//}
 
 Packet* PackPacket(int cmd, char* buffer, int buffer_len) {
 	Packet* pck = (Packet*)malloc(sizeof(PacketHeader) + buffer_len);
@@ -123,5 +176,21 @@ Packet* ParsePacket(char* buffer, int len) {
 		memcpy(&ppck->header, &pck.header, sizeof(PacketHeader));
 		return ppck;
 	}
+}
+
+int InitSocket() {
+	// 客户端网络编程步骤
+	// 1. 初始化socket链接
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	g_server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (g_server_socket == INVALID_SOCKET) {
+		printf("创建服务器socket失败\r\n");
+		return 0;
+	}
+
+	g_server_addr.sin_family = AF_INET; // IPv4协议
+	g_server_addr.sin_port = ntohs(9999); // 转为网络字节序
+	g_server_addr.sin_addr.S_un.S_addr = inet_addr("100.64.54.246"); // 服务器IP地址
 }
 
